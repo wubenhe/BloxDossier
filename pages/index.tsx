0535d3e9800fe9e5c8492b9a387e28ab6fb93f55
Addison Ho <addisonho@armorblox.com>
2021-08-09 02:12:00 +0800

    Add mock API.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import { PolarArea } from "react-chartjs-2";
+import { GetServerSideProps } from "next";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 64px;
   padding: 42px;
+  gap: 16px;
 `;
 
 const PolarWrapper = styled.div`
@@ -14,6 +15,7 @@ const PolarWrapper = styled.div`
   align-items: center;
   height: 500px;
   width: 500px;
+  margin: 48px 0;
 `;
 
 const TitleWrapper = styled.div`
@@ -41,6 +43,7 @@ const Footer = styled.div`
   display: flex;
   width: 100%;
   justify-content: center;
+  margin-top: 48px;
 `;
 
 const Small = styled.span`
@@ -65,7 +68,16 @@ const data = {
   ],
 };
 
-export default function Home() {
+type Props = {
+  placeholder: {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  };
+};
+
+const Home = ({ placeholder }: Props) => {
   const now = new Date().toLocaleDateString();
 
   return (
@@ -77,25 +89,27 @@ export default function Home() {
       <PolarWrapper>
         <PolarArea data={data} />
       </PolarWrapper>
-      <Paragraph>
-        Leverage agile frameworks to provide a robust synopsis for high level
-        overviews. Iterative approaches to corporate strategy foster
-        collaborative thinking to further the overall value proposition.
-        Organically grow the holistic world view of disruptive innovation via
-        workplace diversity and empowerment. Bring to the table win-win survival
-        strategies to ensure proactive domination. At the end of the day, going
-        forward, a new normal that has evolved from generation X is on the
-        runway heading towards a streamlined cloud solution. User generated
-        content in real-time will have multiple touchpoints for offshoring.
-        Capitalize on low hanging fruit to identify a ballpark value added
-        activity to beta test. Override the digital divide with additional
-        clickthroughs from DevOps. Nanotechnology immersion along the
-        information highway will close the loop on focusing solely on the bottom
-        line.
-      </Paragraph>
+      <Paragraph>{placeholder.body}</Paragraph>
+      <Paragraph>{placeholder.body}</Paragraph>
+      <Paragraph>{placeholder.body}</Paragraph>
       <Footer>
         <Small>🗄️ Crafted with BloxDossier</Small>
       </Footer>
     </Container>
   );
-}
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const number = Math.floor(Math.random() * 100);
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${number}`
+  );
+  const placeholder: Props = await res.json();
+  return {
+    props: {
+      placeholder,
+    },
+  };
+};
+
+export default Home;
